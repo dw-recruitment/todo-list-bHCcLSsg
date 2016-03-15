95a9de596830f0edf8d718929c408082b78f882a
Anonymous Candidate <anon@example.com>
2016-03-15 17:09:06 -0500

    Complete step 2; under construction

--- src/ducks/routes/home.clj
+++ src/ducks/routes/home.clj
@@ -1,9 +1,14 @@
 (ns ducks.routes.home
   (:require [compojure.core :refer :all]
+            [hiccup.element :as hiccup-elements]
             [ducks.views.layout :as layout]))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common [:h1 "Under Construction!"]
+                 ;; Image shamelessly stolen from
+                 ;; http://www.cheops-pyramide.ch/khufu-pyramid/great-pyramid/khufu-construction.gif
+                 (hiccup-elements/image "img/khufu-construction.gif" "Image of Khufu Pyramid under construction")
+                 ))
 
 (defroutes home-routes
   (GET "/" [] (home)))

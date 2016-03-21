65ef7248ab48fd743140367862ef362ef97b3a05
Anonymous Candidate <anon@example.com>
2016-03-21 17:24:05 -0500

    echos messages from rabbitmq, quits after 30 sec.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject loros "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Echos rabbitmq messages to console."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+  		[com.novemberain/langohr "3.5.0"]]
   :main ^:skip-aot loros.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/loros/core.clj
+++ src/loros/core.clj
@@ -1,7 +1,33 @@
 (ns loros.core
-  (:gen-class))
+  "Shameless stolen from http://clojurerabbitmq.info/articles/getting_started.html"
+  (:gen-class) 
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.queue     :as lq]
+            [langohr.consumers :as lc]
+            [langohr.basic     :as lb])) 
+
+
+(def ^{:const true}
+  default-exchange-name "")
+
+(defn message-handler
+  [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
+  (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
+                   (String. payload "UTF-8") delivery-tag content-type type)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [conn  (rmq/connect)
+        ch    (lch/open conn)
+        qname "langohr.examples.hello-world"]
+    (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+    (lq/declare ch qname {:exclusive false :auto-delete true})
+    (lc/subscribe ch qname message-handler {:auto-ack true})
+    (lb/publish ch default-exchange-name qname "¡Hola!" {:content-type "text/plain" :type "greetings.hi"})
+    (Thread/sleep 30000)
+    (println "[main] Disconnecting...")
+    (rmq/close ch)
+    (rmq/close conn)))
+
